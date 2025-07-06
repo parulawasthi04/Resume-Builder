c3edecdf24c65dbc2d3ce09405f67ffe521ee08a
Parul Awasthi <parulawasthi2003@gmail.com>
2025-07-06 13:09:47 +0530

    Frontend URL edit

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(
   '/uploads',
   express.static(path.join(__dirname, 'uploads'), {
     setHeaders: (res, _path) => {
-      res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+      res.set('Access-Control-Allow-Origin', 'https://resume-builder-frontend-p3b0.onrender.com');
     },
   })
 );
